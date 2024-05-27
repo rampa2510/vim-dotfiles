c5762ac7bc4a960c4a1710d5edfccee36318d670
ram pandey <iamram2510@gmail.com>
2024-05-27 15:26:48 +0530

    feat(regitser): added map to copy full file

--- init.lua
+++ init.lua
@@ -111,6 +111,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- misc commands
+vim.keymap.set('n', '<C-c>', '<cmd> %y+ <CR>', { desc = 'Copy whole File' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
